14c0e11f2dabfd746ccf9659ac6bb3b35d1ffdb9
root <root@BOOK-QTP3KTPNKO>
2023-12-01 17:24:39 -0800

    Commenting ideas but still not working

--- api/flask_api.py
+++ api/flask_api.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_restful import Api
 from mongoengine import connect
 
+
 # You need to implement two resources: Brevet and Brevets.
 # Uncomment when done:
 from resources.brevet import Brevet
@@ -19,11 +20,13 @@ connect(host=f"mongodb://{os.environ['MONGODB_HOSTNAME']}:27017/brevetsdb")
 # Start Flask app and Api here:
 app = Flask(__name__)
 api = Api(app)
+################
 
 # Bind resources to paths here:
 # api.add_resource(...)
-api.add_resource(Brevet, "/api/brevet/<string:brevet_id>") # brevet resource
-api.add_resource(Brevets, "/api/brevets") # brevets resource
+api.add_resource(Brevet, "/api/brevet/<string:brevet_id>") 
+# I think <string:brevet_id> will let it be variable
+api.add_resource(Brevets, "/api/brevets") 
 
 
 
